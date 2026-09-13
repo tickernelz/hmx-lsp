99f1dd87f5a6b792b07c5d227d224fa84ef738f3
Zhafron <zhafronadani@gmail.com>
2026-09-13 15:06:38 +0700

    chore(release): bump every declaration to 0.2.2

--- editors/jetbrains/build.gradle.kts
+++ editors/jetbrains/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.hashmicro.hmx"
-version = providers.gradleProperty("pluginVersion").getOrElse("0.2.1")
+version = providers.gradleProperty("pluginVersion").getOrElse("0.2.2")
 
 repositories {
     mavenCentral()
