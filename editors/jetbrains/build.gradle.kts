dfb303443578772168c758fb0a0026c03f36f836
Zhafron <zhafronadani@gmail.com>
2026-09-14 11:00:49 +0700

    chore(release): bump every declaration to 0.3.4

--- editors/jetbrains/build.gradle.kts
+++ editors/jetbrains/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.hashmicro.hmx"
-version = providers.gradleProperty("pluginVersion").getOrElse("0.3.3")
+version = providers.gradleProperty("pluginVersion").getOrElse("0.3.4")
 
 repositories {
     mavenCentral()
